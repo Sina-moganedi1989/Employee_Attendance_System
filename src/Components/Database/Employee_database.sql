db4a85b2b6b609020aed247ad7d9489c27cdfda3
ANDIGEMAFABRICEFOMBET <andigemafabrice@gmail.com>
2025-08-06 21:31:17 +0100

    Fabrice added Leave Table

--- src/Components/Database/Employee_database.sql
+++ src/Components/Database/Employee_database.sql
@@ -11,6 +11,7 @@ CREATE TABLE employees (
     status ENUM('active', 'inactive') DEFAULT 'active'
 );
 
+<<<<<<< Updated upstream
 CREATE TABLE attendance (
     attendanceID INT AUTO_INCREMENT PRIMARY KEY,
     employeeID INT,
@@ -18,5 +19,16 @@ CREATE TABLE attendance (
     checkInTime TIME,
     checkOutTime TIME,
     status ENUM('Present', 'Absent', 'On Leave', 'Late') DEFAULT 'Present',
+    FOREIGN KEY (employeeID) REFERENCES Employees(employeeID)
+);
+
+=======
+>>>>>>> Stashed changes
+CREATE TABLE Leave (
+    type VARCHAR(50),
+    sartDate DATE,
+    enddate  DATE,
+    Approval Status VARCHAR(50),
+    reason VARCHAR(50),
     FOREIGN KEY (employeeID) REFERENCES Employees(employeeID)
 );
